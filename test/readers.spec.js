3218abf9afe3f05caeeba01110b7503ee82178b4
Mac Heller-Ogden <mac@machellerogden.com>
2019-08-28 21:00:19 -0500

    entomology

--- test/readers.spec.js
+++ test/readers.spec.js
@@ -22,6 +22,11 @@ test('readers - state non-arn array length 2', t => {
     t.deepEqual(state([ '$.foo', 'foo' ]), { Name: 'foo', Type: 'Task', Resource: 'foo', InputPath: '$.foo' });
 });
 
+test('readers - state non-arn array length 2 - alt order', t => {
+    const { state } = Readers();
+    t.deepEqual(state([ 'foo', '$.foo' ]), { Name: 'foo', Type: 'Task', Resource: 'foo', ResultPath: '$.foo' });
+});
+
 test('readers - state arn array length 1', t => {
     const { state } = Readers();
     t.deepEqual(state([ 'arn:aws:foo' ]), { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo' });
@@ -37,23 +42,31 @@ test('readers - state arn array length 2', t => {
     t.deepEqual(state([ '$.foo', 'arn:aws:foo' ]), { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo' });
 });
 
+test('readers - state arn array length 2 - alt order', t => {
+    const { state } = Readers();
+    t.deepEqual(state([ 'arn:aws:foo', '$.foo'  ]), { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', ResultPath: '$.foo' });
+});
+
 test('readers - state non-arn array length 3', t => {
     const { state } = Readers();
-    t.deepEqual(state([ 'foo', '$.foo', '$.bar' ]), { Name: 'foo', Type: 'Task', Resource: 'foo', InputPath: '$.foo', ResultPath: '$.bar' });
-    // fix me - following should be actual test
-    // t.deepEqual(state([ '$.foo', 'foo', '$.bar' ]), { Name: 'foo', Type: 'Task', Resource: 'foo', InputPath: '$.foo', ResultPath: '$.bar' });
+    t.deepEqual(state([ '$.foo', 'foo', '$.bar' ]), { Name: 'foo', Type: 'Task', Resource: 'foo', InputPath: '$.foo', ResultPath: '$.bar' });
 });
 
 test('readers - state arn array length 3', t => {
     const { state } = Readers();
-    t.deepEqual(state([ 'arn:aws:foo', '$.foo', '$.bar' ]), { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo', ResultPath: '$.bar' });
-    // fix me - following should be actual test
-    // t.deepEqual(state([ '$.foo', 'arn:aws:foo', '$.bar' ]), { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo', ResultPath: '$.bar' });
+    t.deepEqual(state([ '$.foo', 'arn:aws:foo', '$.bar' ]), { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo', ResultPath: '$.bar' });
 });
 
-test('readers - state object', t => {
+test('readers - state object - literal props', t => {
     const { state } = Readers();
     const input =  { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo', ResultPath: '$.bar' };
+    const output = { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo', ResultPath: '$.bar' };
+    t.deepEqual(state(input), output);
+});
+
+test('readers - state object - shorthand props', t => {
+    const { state } = Readers();
+    const input =  { n: 'foo', t: 'Task', r: 'arn:aws:foo', i: '$.foo', resu: '$.bar' };
     const output = { Name: 'foo', Type: 'Task', Resource: 'arn:aws:foo', InputPath: '$.foo', ResultPath: '$.bar' };
     t.deepEqual(state(input), output);
 });
